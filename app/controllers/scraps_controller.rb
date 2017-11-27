f7d6d3a64249557b6c75e8d7c1bacce4125e1f8f
Dalma Boros <dalma.boros@gmail.com>
2017-11-27 07:22:47 -0600

    Create scrap form

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -1,2 +1,27 @@
 class ScrapsController < ApplicationController
+
+  def new
+    @scrap = Scrap.new
+  end
+
+  def create
+    @scrap = Scrap.new(scrap_params)
+
+    if @scrap.save
+      redirect_to @scrap
+    else
+      render :new
+    end
+  end
+
+  def show
+    #
+  end
+
+  private
+
+  def scrap_params
+    params.require(:scrap).permit(:description, :category)
+  end
+
 end
