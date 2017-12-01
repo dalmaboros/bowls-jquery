8d5c8e92ed1bb073ffe339b2b4d0cc91809efbdc
Dalma Boros <dalma.boros@gmail.com>
2017-11-30 18:09:27 -0600

    Added length validation for scrap and bowl names

--- app/models/bowl.rb
+++ app/models/bowl.rb
@@ -2,7 +2,7 @@ class Bowl < ActiveRecord::Base
   has_many :bowl_scraps
   has_many :scraps, through: :bowl_scraps
 
-  validates :name, presence: :true, uniqueness: :true
+  validates :name, presence: :true, uniqueness: :true, length: { maximum: 40 }
 
   belongs_to :user
 
--- app/models/scrap.rb
+++ app/models/scrap.rb
@@ -2,7 +2,7 @@ class Scrap < ActiveRecord::Base
   has_many :bowl_scraps
   has_many :bowls, through: :bowl_scraps
 
-  validates :description, presence: :true, uniqueness: :true
+  validates :description, presence: :true, uniqueness: :true, length: { maximum: 140 }
 
   belongs_to :user
 end
