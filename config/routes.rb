a60a84f111a2f7258814effc37d28156bbc74d1a
Dalma Boros <dalma.boros@gmail.com>
2017-12-04 20:16:53 -0600

    Omniauth Google

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   resources :bowls do
     resources :scraps, only: [:show, :index, :new, :create]
   end
-  devise_for :users
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
