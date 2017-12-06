240b420cc6b6275b15b5951e3e5fe30044398da4
Dalma Boros <dalma.boros@gmail.com>
2017-12-06 10:10:12 -0600

    All routing errors redirect to '/'

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  get '*path' => redirect('/')
+
   # devise_scope :user do
   #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
