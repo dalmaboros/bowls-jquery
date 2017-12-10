7cb6f2f843a3459fef493393d70e9868c2d5dd57
Dalma Boros <dalma.boros@gmail.com>
2017-12-10 09:20:19 -0600

    Scoped uniqueness of bowl and scrap to user

--- app/models/bowl.rb
+++ app/models/bowl.rb
@@ -2,7 +2,7 @@ class Bowl < ActiveRecord::Base
   has_many :bowl_scraps
   has_many :scraps, through: :bowl_scraps
 
-  validates :name, presence: :true, uniqueness: :true, length: { maximum: 40 }
+  validates :name, presence: :true, :uniqueness => {scope: :user_id}, length: { maximum: 40 }
   validates :user, presence: :true
 
   belongs_to :user
--- app/models/scrap.rb
+++ app/models/scrap.rb
@@ -2,7 +2,7 @@ class Scrap < ActiveRecord::Base
   has_many :bowl_scraps
   has_many :bowls, through: :bowl_scraps
 
-  validates :description, presence: :true, uniqueness: :true, length: { maximum: 140 }
+  validates :description, presence: :true, uniqueness: {scope: :user_id}, length: { maximum: 140 }
 
   belongs_to :user
 end
