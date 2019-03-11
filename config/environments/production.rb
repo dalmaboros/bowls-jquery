31a1d05ab7afecb2a07f93bd74570413e241dc87
Dalma Boros <dalma.boros@gmail.com>
2019-03-11 17:26:34 -0500

    Change 'config.serve_static_files' to 'config.public_file_server.enabled = true'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Following line will be deprecated in Rails 5.1
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Use this instead:
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
